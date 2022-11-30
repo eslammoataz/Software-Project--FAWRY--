375329e9892008a899fc19095ee8c8ef3d1c7e18
eslammoataz <eslammezo2323@gmail.com>
2022-11-30 16:05:52 +0200

    main class commit

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Dsdds");
+        System.out.println("Setup of the new repo in github ... Commit ");
     }
 }
